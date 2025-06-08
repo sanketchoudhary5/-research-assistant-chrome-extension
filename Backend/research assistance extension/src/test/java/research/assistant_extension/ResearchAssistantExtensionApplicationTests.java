582b875eada042a9c8a97a26cf6eb74fab7dd886
Sanket Choudhary <choudharysanket13@gmail.com>
2025-06-08 17:35:07 +0530

    Add files via upload

---
+++ Backend/research assistance extension/src/test/java/research/assistant_extension/ResearchAssistantExtensionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.research.assistant_extension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchAssistantExtensionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
